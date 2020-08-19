6f0907eec71a4a4c10da919fbd37b682e7ae2b54
mnayaz <69887351+mnayaz@users.noreply.github.com>
2020-08-19 10:24:03 -0700

    Updated Some Content

--- src/main/java/HelloWorld/App.java
+++ src/main/java/HelloWorld/App.java
@@ -6,6 +6,16 @@ package HelloWorld;
  */
 public class App 
 {
+    public App()
+    {
+        System.out.println("Constructor");
+    }
+    
+    public void m1()
+    {
+        String s="";
+    }
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
